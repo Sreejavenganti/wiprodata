f5ac1fd65a7cfba026893cb12644074235ce5c0e
Sreejavenganti <sreeja27148@gmail.com>
2025-07-22 23:31:26 +0530

    first commit

---
+++ DemoPractice/DemoPractice/Ladder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPractice
+{
+    internal class Ladder
+    {
+        public void Show(int choice)
+        {
+            if (choice == 1)
+            {
+                Console.WriteLine("Hi I am Durga...");
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Hi I am Parvathi...");
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Hi I am Devi...");
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Hi am Radha...");
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Hi I am Rukmini...");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Choice");
+            }
+        }
+        static void Main()
+        {
+            int choice;
+            Console.WriteLine("Enter Your Choice  ");
+            choice = Convert.ToInt32(Console.ReadLine());
+            Ladder ladder = new Ladder();
+            ladder.Show(choice);
+        }
+    }
+}
---
+++ DemoPractice/DemoPractice/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPractice
+{
+    internal class Test
+    {
+        static void Main()
+        {
+            int a, b, c;
+            Console.WriteLine("Enter  2 numbers ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Sum is " +c);
+            c = a - b;
+            Console.WriteLine("Sub is " + c);
+            c = a * b;
+            Console.WriteLine("Mult is " + c);
+        }
+    }
+}
